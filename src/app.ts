85a84949397c648b43b2ae50fa289aa018b3d12d
Yinon Bar <yinonbar1988@gmail.com>
2023-08-07 22:35:35 +0300

    Access-Control-Allow-Origin

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import router from "./6-controllers/attendees-controller";
+import cors from "cors";
 
 const server = express();
 
 server.use(express.json());
+
+server.use(
+    cors({
+        origin: "*",
+    })
+);
 
 server.use(router);
 
