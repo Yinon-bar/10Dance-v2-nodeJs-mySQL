59f6b211468a0df2478f6df2e6b4780e3515d7e3
ינון בר <yinonb@hebrew-academy.org.il>
2023-08-10 11:10:09 +0300

    db config

--- dist/6-controllers/attendees-controller.js
+++ dist/6-controllers/attendees-controller.js
@@ -15,24 +15,33 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const attendee_logic_1 = __importDefault(require("../5-logic/attendee-logic"));
 const router = express_1.default.Router();
+// Get try
+router.get("/", (req, resp, next) => __awaiter(void 0, void 0, void 0, function* () {
+    resp.header("Access-Control-Allow-Origin", "*");
+    resp.json("hello");
+}));
 // Get all Tables
 router.get("/api/tables", (req, resp, next) => __awaiter(void 0, void 0, void 0, function* () {
+    resp.header("Access-Control-Allow-Origin", "*");
     const tables = yield attendee_logic_1.default.getAllTables();
     resp.json(tables);
 }));
 // Get all from specific table
 router.get("/api/:table", (req, resp, next) => __awaiter(void 0, void 0, void 0, function* () {
+    resp.header("Access-Control-Allow-Origin", "*");
     const tableName = req.params.table;
     const attFromTable = yield attendee_logic_1.default.getAllAttFromTable(tableName);
     resp.json(attFromTable);
 }));
 // Get all attendees
 router.get("/api/attendees", (req, resp, next) => __awaiter(void 0, void 0, void 0, function* () {
+    resp.header("Access-Control-Allow-Origin", "*");
     const attendees = yield attendee_logic_1.default.getAllAttendees();
     resp.json(attendees);
 }));
 // Get by tz_id
 router.get("/api/attendees/:tz_id", (req, resp, next) => __awaiter(void 0, void 0, void 0, function* () {
+    resp.header("Access-Control-Allow-Origin", "*");
     const attendee = yield attendee_logic_1.default.getAttendeeById(req.params.tz_id);
     resp.json(attendee);
 }));
--- dist/app.js
+++ dist/app.js
@@ -5,8 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const attendees_controller_1 = __importDefault(require("./6-controllers/attendees-controller"));
+const cors_1 = __importDefault(require("cors"));
 const server = (0, express_1.default)();
 server.use(express_1.default.json());
+server.use((0, cors_1.default)({ origin: "*" }));
 server.use(attendees_controller_1.default);
 server.listen(3001, () => {
     console.log("Listening to http://localhost:3001");
